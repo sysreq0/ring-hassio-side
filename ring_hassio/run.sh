dbc0bd8fd2567d5a3a9b98324583193f1b5a3c6a
Sebastien Couture <scouture@bandwidth.com>
2021-09-23 20:43:02 -0400

    third commit

--- ring_hassio/run.sh
+++ ring_hassio/run.sh
@@ -4,5 +4,5 @@ export RING_REFRESH_TOKEN="$(jq --raw-output '.ring_refresh_token' $CONFIG_PATH)
 export CAMERA_NAME="$(jq --raw-output '.camera_name' $CONFIG_PATH)"
 export RING_PORT="$(jq --raw-output '.port' $CONFIG_PATH)"
 
-cd /ring-hassio/ring_hassio
+cd /ring-hassio-side/ring_hassio
 node livestream.js
